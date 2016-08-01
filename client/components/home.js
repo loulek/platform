87ac1e6c233dfd716fa6041d6dec54897768a467
Louis B <louisbiret@gmail.com>
2016-08-01 15:27:00 -0400

    profile data defined

--- client/components/home.js
+++ client/components/home.js
@@ -18,7 +18,7 @@ class App extends React.Component {
 		var userContent = <div>
 							<h3>Bienvenue</h3>
 							<hr />
-							<EditEvent />
+							<CreateEvent />
 						  </div>;
 
 		var content = null;
@@ -36,23 +36,21 @@ class App extends React.Component {
 }
 
 
-class EditEvent extends React.Component {
+class CreateEvent extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			profileData: {
-				email: null,
-				gender: null,
-				firstName: null,
-				lastName: null,
-				phone: null,
-				specialty: [],
-				image: null,
-				description: null,
-				profileImageUrl: null,
-				resumeImageUrl: null,
-				address: null
+			eventData: {
+				title: null,
+				location: [],
+				address: null,
+				startDate: null,
+				endDate: null,
+				startHour: null,
+				endHour: null,
+				workerNumber: null,
+				budget: null	
 			},
 			tempSpecialty: [],
 			editContact: false,
@@ -62,7 +60,7 @@ class EditEvent extends React.Component {
 	}
 
 
-_editContact(isEnabled) {
+_createEvent(isEnabled) {
 	var that = this
 			return (
 				<div className='panel panel-default'>
@@ -71,39 +69,50 @@ _editContact(isEnabled) {
 					</div>
 					<div className='panel-body'>
 						<div className="form-group row">
-							<p className="col-sm-2 form-control-static"><b>Nom de l'événement:</b></p>
-							<div className="col-sm-10">
-								<input type="text" className="form-control" name="firstName" defaultValue={this.state.profileData.firstName} id="firstName"/>
+							<p className="col-sm-2 form-control-static"><b>Nom d'Événement:</b></p>
+							<div className="col-sm-8">
+								<input type="text" className="form-control" name="firstName" defaultValue={this.state.eventData.title} id="title"/>
 							</div>
 						</div>
 						<div className="form-group row">
-							<p className="col-sm-2 form-control-static"><b>Lieu:</b></p>
-							<div className="col-sm-10">
-								<input type="text" className="form-control" name="lastName" defaultValue={this.state.profileData.lastName} id="lastName"/>
+							<p className="col-sm-2 form-control-static"><b>Adresse:</b></p>
+							<div className="col-sm-8">
+
+							<Geosuggest inputClassName="form-control" placeholder="" initialValue={this.state.eventData.address} id="address" />
+
+							</div>
+						</div>
+						<div className="form-group row">
+							<p className="col-sm-2 form-control-static"><b>Nombre d'Hôtes(ses):</b></p>
+							<div className="col-sm-8">
+								<select className="form-control" defaultValue={this.state.eventData.workerNumber} id="workerNumber">
+									<option value='1'>1</option>
+									<option value='2'>2</option>
+									<option value='3'>3</option>
+									<option value='4'>4</option>
+									<option value='5'>5</option>
+									<option value='6'>6</option>
+									<option value='7'>7</option>
+									<option value='8'>8</option>
+									<option value='9'>9</option>
+									<option value='10+'>10+</option>
+								</select>
 							</div>
 						</div>
 						<div className="form-group row">
 						<p className="col-sm-2 form-control-static"><b>Date:</b></p>
 						<div className="col-sm-10">
-							<p className="form-control-static">{this.state.profileData.email}</p>
+							<p className="form-control-static">{this.state.eventData.email}</p>
 						</div>
 						</div>
 						<div className="form-group row">
 							<p className="col-sm-2 form-control-static"><b>Heures de travail:</b></p>
 							<div className="col-sm-10">
-								<input type="text" className="form-control" name="phone" defaultValue={this.state.profileData.phone} id="phone"/>
-							</div>
-						</div>
-						<div className="form-group row">
-							<p className="col-sm-2 form-control-static"><b>Nombre d'employés:</b></p>
-							<div className="col-sm-8">
-
-							<Geosuggest inputClassName="form-control" placeholder="" initialValue={this.state.profileData.address} id="address" />
-
+								<input type="text" className="form-control" name="phone" defaultValue={this.state.eventData.phone} id="phone"/>
 							</div>
 						</div>
 						<button className="btn btn-success margin5 float-right" onClick={that._updateContact}>Save</button>
-						<button className="btn btn-warning margin5 float-right" onClick={function() {this.setState({editContact: false, tempSpecialty:[]})}.bind(this)}>Cancel</button>
+						<button className="btn btn-warning margin5 float-right" onClick={function() {this.setState({CreateEvent: false, tempSpecialty:[]})}.bind(this)}>Cancel</button>
 					</div>
 				</div>
 			);
@@ -114,9 +123,9 @@ render() {
 
 		var contactForm = null;
 		if(this.state.editContact) {
-			contactForm = this._editContact(true);
+			contactForm = this._createEvent(true);
 		} else {
-			contactForm = this._editContact(false);
+			contactForm = this._createEvent(false);
 		}
 
 

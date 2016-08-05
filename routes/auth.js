f47c7cba177f95bfb57a977afd90e955953fd56b
Louis B <louisbiret@gmail.com>
2016-08-05 10:12:49 -0400

    email send grid

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
-
 var path = require('path');
+var sg = require('sendgrid')(process.env.SENDGRID_APIKEY);
+var helper = require('sendgrid').mail;
+
+var sendEmail = function(options) {
+	from_email = new helper.Email("louisbiret@gmail.com")
+	to_email = new helper.Email(options.email)
+	subject = "Email de confirmation"
+	content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js")
+	mail = new helper.Mail(from_email, subject, to_email, content)
+	console.log("EMAIL1", options.email)
+
+	console.log("EMAIL2", options.email)
+
+	var requestBody = mail.toJSON()
+
+	console.log(requestBody)
+	var request = sg.emptyRequest({
+		method: 'POST',
+		path: '/v3/mail/send',
+		body: requestBody
+	})
+	sg.API(request, function (error, response) {
+	  console.log(response.statusCode)
+	  console.log(response.body)
+	  console.log(response.headers)
+	})
+}
 
 module.exports = function(passport) {
 
@@ -11,6 +37,9 @@ module.exports = function(passport) {
 		if (req.body.password !== req.body.repeatPassword){
 			return next("Passwords did not match")
 		}
+		sendEmail({
+			email: req.body.email
+		});
 		var user = new User ({
 			email : req.body.email,
 			password : User.generateHash(req.body.password),
@@ -28,6 +57,11 @@ module.exports = function(passport) {
 		if (req.body.password !== req.body.repeatPassword){
 			return next("Passwords did not match")
 		}
+
+		sendEmail({
+			email: req.body.email
+		});
+
 		var user = new User ({
 			email : req.body.email,
 			password : User.generateHash(req.body.password),

9381579339a7b3a59e0c0e5940201c403e5411b1
Z-Lu <zl2da@virginia.edu>
2016-08-11 12:24:20 -0400

    combine two buttons and fix calendar writing to database

--- models/availability.js
+++ models/availability.js
@@ -1,17 +1,11 @@
 var mongoose = require("mongoose");
 
 var availabilitySchema = mongoose.Schema ({
-	month: {
-		type: Number
-	},
-	week: {
-		type: Number
-	},
 	times: {
 		type: Array
 	},
 	user: {
-		type: mongoose.Schema.Types.ObjectId, 
+		type: mongoose.Schema.Types.ObjectId,
 		ref: "Profile"
 	}
 });
